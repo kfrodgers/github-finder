54c4901ae97f9f9baa9cfce3853e4e0c5a7e22f0
kfrodgers <kevin@rodgersworld.com>
2022-05-11 14:14:16 -0400

    finished 3.1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component} from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
-    foo = () => 'Bari';
     render() {
-        const name = 'John Doe';
-        const loading = false;
-        if (loading) {
-            return <h4>Loading</h4>;
-        }
         return (
             <div className="App">
-                <h1>Hello {name} {this.foo()}</h1>
+                <Navbar />
             </div>
         );
     }
